0cf6e26e1bae83a31f6a47f8271c1cf48d7f7699
Balajihero07 <158764057+Balajihero07@users.noreply.github.com>
2024-08-29 22:51:09 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,10 @@ const authController = require('../controllers/authcontroller');
 const authMiddleware = require('../middleware/authmiddleware');
 
 // User Registration
-router.post('/register', authController.registerFromURL);
+router.post('https://murugan-yram.onrender.com/register', authController.registerFromURL);
 
 // User Login
-router.post('/login', authController.login);
+router.post('https://murugan-yram.onrender.com/login', authController.login);
 
 // Create Wedding Order (Protected Route)
 router.post('/weddorder', authMiddleware, authController.createWeddingOrder);
