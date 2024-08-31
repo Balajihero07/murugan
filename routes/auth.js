7433f8bb75f033ce774167e6568c511a6a742929
Balajihero07 <158764057+Balajihero07@users.noreply.github.com>
2024-08-31 12:27:18 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,10 @@ const authMiddleware = require('../middleware/authmiddleware');
 router.post('/register', authController.registerFromURL);
 
 // User Login
-router.post('https://murugan-14q1.onrender.com/login', authController.login);
+router.post('/login', authController.login);
 
 // Create Wedding Order (Protected Route)
-router.post('https://murugan-14q1.onrender.com/weddorder', authMiddleware, authController.createWeddingOrder);
+router.post('/weddorder', authMiddleware, authController.createWeddingOrder);
 //CREATE ABIRTHDAY ORDER
 router.post('/brithdayorder', authMiddleware, authController.createbrithdayOrder);
 //CREATE A HOUSE ORDER
