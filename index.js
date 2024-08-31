ee46010280a1036d6557f39bce09564d6636e4fc
Balajihero07 <158764057+Balajihero07@users.noreply.github.com>
2024-08-31 20:50:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,102 @@ app.get('/', (req, res) => {
   res.set({ 'Access-Control-Allow-Origin': '*' });
   res.redirect('/index.html');
 });
+// Get Order by ID
+app.get('/api/auth/weddorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
 
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+app.get('/api/auth/brithdayorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+app.get('/api/auth/houseorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+app.get('/api/auth/babyshowerorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+app.get('/api/auth/festivalorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+app.get('/api/auth/partyorder/:id', async (req, res) => {
+  const orderId = req.params.id;
+  
+  try {
+    const order = await Order.findById(orderId); // Fetch the order using the Order model
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).send('Order not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
