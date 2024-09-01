46dedb287a600880040bcb8499eb0a6eaf99359a
Balajihero07 <158764057+Balajihero07@users.noreply.github.com>
2024-09-01 13:16:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
